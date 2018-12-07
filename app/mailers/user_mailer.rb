bcc7c28a6cfb552f5eeabf3bfd8b39e4eb2e9a20
Mark Barlescu <barlescu92@gmail.com>
2018-12-07 23:20:41 +0000

    finally works, contents in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ default from: 'no-reply@jungle.com'
   def welcome_email(order)
     @order = order
     @url  = 'http://example.com/login'
-    mail(to: 'barlescu92@gmail.com', subject: 'Welcome to My Awesome Site')
+    @message = 'yo wattup'
+    mail(to: @order.email, subject: 'Welcome to My Awesome Site')
   end
 end
