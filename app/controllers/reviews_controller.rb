820adf611b7fae42012d5270da8a35afd3cd444a
Mark Barlescu <barlescu92@gmail.com>
2018-12-08 05:27:50 +0000

    form for reviews for products works

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,35 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @product = Product.find params[:product_id]
+    @new_review_params = {
+      "user_id" => @current_user.id,
+      "product_id" => params[:product_id],
+      "description" => review_params[:description],
+      "rating" => review_params[:rating]
+    }
+    review = Review.new(@new_review_params)
+    review.save
+    redirect_to @product
+
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+      )
+  end
+
+end
+
+
+  # validates :product_id, presence: true
+  # validates :user_id, presence: true
+  # validates :description, presence: true
+  # validates :rating, presence: true
